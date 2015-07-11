c2c1c4fc7e75cb7deaae3cb8c3fa552e1dc14158
Josef Friedrich <josef@friedrich.rocks>
2015-07-11 22:19:01 +0200

    Add source.

--- ring.sh
+++ ring.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
-TITLE=''
+TITLE='Ring'
 DESCRIPTION=''
-SOURCE=''
-beep
+SOURCE='http://www.reddit.com/r/linux/comments/18h8v5/does_anyone_have_or_know_a_source_for_beep_scripts/'
+for n in 1 2 3 ; do
+    for f in 1 2 1 2 1 2 1 2 1 2 ; do
+      beep -f ${f}000 -l 20
+    done
+done
--- vader-jacob.sh
+++ vader-jacob.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
-TITLE=''
+TITLE='Vader jacob'
 DESCRIPTION=''
-SOURCE=''
-beep
+SOURCE='http://www.reddit.com/r/linux/comments/18h8v5/does_anyone_have_or_know_a_source_for_beep_scripts/'
+beep -l 300 -f 100 -n -l 300 -f 112 -n -l 300 -f 126 -n -l 300 -f 100 -n -l 300 \
+                -f 100 -n -l 300 -f 112 -n -l 300 -f 126 -n -l 300 -f 100 -n -l 300 \
+                -f 126 -n -l 300 -f 133 -n -l 600 -f 150 -n -l 300 -f 126 -n -l 300 \
+                -f 133 -n -l 600 -f 150 -n -l 150 -f 150 -n -l 150 -f 168 -n -l 150 \
+                -f 150 -n -l 150 -f 133 -n -l 300 -f 126 -n -l 300 -f 100 -n -l 150 \
+                -f 150 -n -l 150 -f 168 -n -l 150 -f 150 -n -l 150 -f 133 -n -l 300 \
+                -f 126 -n -l 300 -f 100 -n -l 300 -f 100 -n -l 300 -f 75 -n -l 600 \
+                -f 100 -n -l 300 -f 100 -n -l 300 -f 75 -n -l 600 -f 100
