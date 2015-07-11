3c72c22e0bbb0e26e6affed2e037361daac8cec0
Josef Friedrich <josef@friedrich.rocks>
2015-07-11 19:40:22 +0200

    First melodies.

---
+++ axel-f.sh
@@ -0,0 +1,6 @@
+#! /bin/sh
+TITLE='Axel F'
+DESCRIPTION=''
+SOURCE='http://blog.meyju.net/2011/07/27/beep-sound'
+
+beep -f 659 -l 460 -n -f 784 -l 340 -n -f 659 -l 230 -n -f 659 -l 110 -n -f 880 -l 23 -n -f 659 -l 230 -n -f 587 -l 230 -n -f 659 -l 460 -n -f 988 -l 340 -n -f 659 -l 230 -n -f 659 -l 110 -n -f 1047 -l 230 -n -f 988 -l 230 -n -f 784 -l 230 -n -f 659 -l 230 -n -f 988 -l 230 -n -f 1318 -l 230 -n -f 659 -l 110 -n -f 587 -l 230 -n -f 587 -l 110 -n -f 494 -l 230 -n -f 740 -l 230 -n -f 659 -l 460
