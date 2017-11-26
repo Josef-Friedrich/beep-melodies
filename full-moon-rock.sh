5981d07211adf74aaa049881ee02e7ca71381d46
Josef Friedrich <josef@friedrich.rocks>
2017-11-26 08:46:22 +0100

    Add new melodies

---
+++ full-moon-rock.sh
@@ -0,0 +1,76 @@
+#! /bin/sh
+TITLE='Full moon rock'
+DESCRIPTION='For those who remember the Amiga module full moon rock – here is a juicy little nugget for ya'
+SOURCE='https://www.kirrus.co.uk/2010/09/linux-beep-music/'
+
+#Note Frequency
+LA1=233.1
+LG=196
+
+C=261.6
+C1=277.2
+D=293.7
+D1=311.1
+E=329.6
+F=349.2
+F1=370.0
+G=392.0
+G1=415.3
+A=440.0
+A1=466.2
+B=493.9
+C2=523.2
+
+C22=554.3
+D2=587.33
+D12=622.2
+E2=659.26
+F2=698.46
+F22=739.99
+G2=783.99
+G22=830.61
+A2=880.00
+A22=932.33
+B2=987.77
+C3=1046.50
+
+beep -f $C -l 50
+beep -f 20 -l 150
+beep -f $C1 -l 50
+beep -f 20 -l 150
+beep -f $F1 -l 50
+beep -f 20 -l 50
+beep -f $G -l 50
+beep -f 20 -l 150
+beep -f $LA1 -l 50
+beep -f 20 -l 50
+beep -f $E -l 50
+beep -f 20 -l 150
+beep -f $LG -l 50
+beep -f 20 -l 150
+beep -f $C -l 50
+beep -f 20 -l 50
+beep -f $LA1 -l 50
+beep -f 20 -l 50
+beep -f $C1 -l 50
+beep -f 20 -l 150
+beep -f $C1 -l 50
+beep -f 20 -l 150
+beep -f $F1 -l 50
+beep -f 20 -l 150
+beep -f $G1 -l 50
+beep -f 20 -l 50
+beep -f $LA1 -l 50
+beep -f 20 -l 150
+beep -f $E -l 50
+beep -f 20 -l 50
+beep -f $G -l 50
+beep -f 20 -l 150
+beep -f $F -l 50
+beep -f 20 -l 150
+beep -f $LA1 -l 50
+beep -f 20 -l 50
+beep -f $C1 -l 50
+beep -f $D -l 50
+beep -f $C1 -l 50
+beep -f 20 -l 150
