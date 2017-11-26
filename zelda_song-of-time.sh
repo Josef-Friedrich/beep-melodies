72082597679897d2377f91829aaf941b9857e317
Josef Friedrich <josef@friedrich.rocks>
2017-11-26 09:05:16 +0100

    Add a more describtive title

--- zelda_song-of-time.sh
+++ zelda_song-of-time.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
-TITLE='Zelda'
+TITLE='Zelda - Song of times'
 DESCRIPTION=''
 SOURCE='http://www.kirrus.co.uk/2010/09/linux-beep-music/'
+# https://github.com/ShaneMcC/beeps/blob/master/songoftime.sh
 beep	-f880	-l500	-D70	\
 -n	-f587.33	-l1000	-D70	\
 -n	-f698.46	-l500	-D70	\
